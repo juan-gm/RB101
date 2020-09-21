bd75d60e02ae20dd646c29ab2b213952f4b32409
Juan Garcia <srgarci79@gmail.com>
2020-09-21 12:05:40 +0200

    Mortgage calculator

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -2,28 +2,70 @@ def prompt(message)
   puts("=> #{message}")
 end
 
-prompt("Welcome to this fantastic mortgage calculator!")
+def valid?(input)
+  ((input.to_f.to_s == input) ||
+    (input.to_i.to_s == input)) &&
+    (input.to_f > 0.0)
+end
 
-prompt("First, I need to know the total loan amount")
-loan_amount = gets.chomp.to_f
+loop do
+  prompt("Welcome to this fantastic mortgage calculator!")
 
-prompt("Now, tell me the Annual Percentage Rate in %")
-annual_percentage_rate = gets.chomp.to_f
+  loan_amount = ''
+  annual_percentage_rate = ''
+  years = ''
+  months = ''
 
-prompt("Finally, I need to know the loan duration")
-p "years"
-years = gets.chomp.to_i
-months = gets.chomp.to_i
+  loop do
+    prompt("First, I need to know the total loan amount")
+    loan_amount = gets.chomp
+    break if (!loan_amount.empty?) && (valid?(loan_amount))
+    prompt("Please, introduce a non-zero positive number")
+  end
 
-prompt("Calculating...")
-loan_duration_months = years * 12 + months
-monthly_interest_rate = annual_percentage_rate / 1200
-monthly_payment = loan_amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-loan_duration_months)))
+  loop do
+    prompt("Now, tell me the Annual Percentage Rate in %")
+    annual_percentage_rate = gets.chomp
+    break if (!annual_percentage_rate.empty?) &&
+             (valid?(annual_percentage_rate))
+    prompt("Please, introduce a non-zero positive number")
+  end
 
-prompt(monthly_interest_rate.to_s)
-prompt(loan_duration_months.to_s)
-prompt(monthly_payment.to_s)
+  prompt("Finally, I need to know the loan duration")
+  loop do
+    loop do
+      prompt("Years: ")
+      years = gets.chomp
+      break if years.to_i.to_s == years
+      prompt("Please, enter a number as years")
+    end
+    loop do
+      prompt("Months: ")
+      months = gets.chomp
+      break if months.to_i.to_s == months
+      prompt("Please, enter a number as months")
+    end
+    break if years.to_i + months.to_i != 0
+    prompt("Remember that years AND months can't be zero!")
+  end
 
+  prompt("Calculating...")
+  loan_duration_months = years.to_i * 12 + months.to_i
+  monthly_interest_rate = annual_percentage_rate.to_f / 1200
+  monthly_payment = loan_amount.to_f *
+                    (monthly_interest_rate / (1 -
+                    (1 + monthly_interest_rate)**(-loan_duration_months)))
 
+  monthly_interest_rate *= 100
+  prompt("The monthly interest rate is:
+        #{format('%.2f', monthly_interest_rate)}%")
+  prompt("The loan duration is: #{loan_duration_months} months")
+  prompt("The monthly payments are: #{format('%.2f', monthly_payment)} €")
 
+  prompt("Do you want to perform another calculation?
+    Press Y to calculate again")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
 
+prompt("Thank you for using the calculator. Good bye!")
